ed7d21cc511f039a478676ffe478ee4cbcad0f5a
cryptic-bmd <emmybmd@gmail.com>
2026-03-14 19:41:04 +0100

    feat: Telegram Mini App SDK with platform-specific handling

---
+++ client/src/config.ts
@@ -0,0 +1,3 @@
+
+export const PROJECT_NAME: string = import.meta.env.VITE_PROJECT_NAME ?? 'crash';
+export const DEV: boolean = import.meta.env.VITE_DEV == "true";
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
+
+import App from './App.tsx'
+import EnvUnsupported from './components/EnvUnsupported.tsx';
+import { init } from './init.ts';
+import { DEV } from './config.ts';
+
+import './index.css'
+// Include Telegram UI styles first to allow our code override the package CSS.
+import '@telegram-apps/telegram-ui/dist/styles.css';
+
+
+const root = createRoot(document.getElementById('root')!);
+
+try {
+  const launchParams = retrieveLaunchParams();
+  const { tgWebAppPlatform: platform } = launchParams;
+  const debug = (launchParams.tgWebAppStartParam || '').includes('platformer_debug') || DEV;
+
+  // Configure all application dependencies.
+  await init({
+    debug,
+    eruda: debug && ['ios', 'android'].includes(platform),
+    mockForMacOS: platform === 'macos',
+  })
+    .then(() => {
+      root.render(
+        DEV ? (
+          <StrictMode>
+            <App/>
+          </StrictMode>
+        ) : (
+          <App/>
+        )
+      );
+    });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+} catch (e) {
+  root.render(<EnvUnsupported/>);
+}
